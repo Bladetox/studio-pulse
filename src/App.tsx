121a5a53d94f4ee113acba36e9877e9b8988fb6d
Bladetox <152655158+Bladetox@users.noreply.github.com>
2026-03-03 09:22:39 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,9 @@
-import { useEffect, useRef } from 'react'
-import gsap from 'gsap'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
-import { useGSAP } from '@gsap/react'
-import Lenis from 'lenis'
-import Navigation from './components/Navigation'
 import Hero from './components/Hero'
-import LatestArticles from './components/LatestArticles'
-import ArtCategory from './components/ArtCategory'
-import Lifestyle from './components/Lifestyle'
-import Design from './components/Design'
-import GreenTribe from './components/GreenTribe'
-import Authors from './components/Authors'
-import InstagramGallery from './components/InstagramGallery'
-import Footer from './components/Footer'
-
-gsap.registerPlugin(ScrollTrigger)
 
 export default function App() {
-  const lenisRef = useRef<Lenis | null>(null)
-
-  useEffect(() => {
-    const lenis = new Lenis()
-    lenisRef.current = lenis
-
-    lenis.on('scroll', () => ScrollTrigger.update())
-
-    gsap.ticker.add((time) => {
-      lenis.raf(time * 1000)
-    })
-    gsap.ticker.lagSmoothing(0)
-
-    return () => {
-      lenis.destroy()
-      gsap.ticker.remove((time) => lenis.raf(time * 1000))
-    }
-  }, [])
-
   return (
     <div className="min-h-screen bg-dark text-white overflow-x-hidden">
-      <Navigation />
-      <main>
-        <Hero />
-        <LatestArticles />
-        <ArtCategory />
-        <Lifestyle />
-        <Design />
-        <GreenTribe />
-        <Authors />
-        <InstagramGallery />
-      </main>
-      <Footer />
+      <Hero />
     </div>
   )
 }
